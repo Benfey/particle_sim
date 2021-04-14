dedd5cac41f97c328af1517bdcc7d35d78609522
Anton <anton.benfey@gmail.com>
2021-04-14 14:49:07 -0300

    moved colors to a class

---
+++ colors.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Color(Enum):
+    BLACK = (0, 0, 0)
+    RED = (220, 0, 0)
+    GREEN = (0, 100, 0)
+    BLUE = (0, 100, 100)
--- particles.py
+++ particles.py
@@ -3,22 +3,24 @@ import random
 import time
 import sys
 
-BLACK = (0, 0, 0)
-color = (0, random.randint(90,110), random.randint(90,110))
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 1000
-TIMER = pygame.time.get_ticks()
-sand_speed = 12
-block_size = 4
+from colors import Color
 
-def main():
-    global SCREEN, CLOCK, color
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
+block_size = 4
+sand_speed = 12
+particle_color = Color.BLUE.value
+
+def main(w, h):
+    global SCREEN, particle_color, timer
+    holding_mouse = False
+
     pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    SCREEN.fill(BLACK)
     init_grid()
 
-    holding_mouse = False
+    SCREEN = pygame.display.set_mode((w, h))
+    timer = pygame.time.get_ticks()
+    SCREEN.fill(Color.BLACK.value)
 
     while True:
 
@@ -27,13 +29,13 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     init_grid()
-                    SCREEN.fill(BLACK)
+                    SCREEN.fill(Color.BLACK.value)
                 if event.key == pygame.K_1:
-                    color = (207, 16, 32)
+                    particle_color = Color.RED.value
                 if event.key == pygame.K_2:
-                    color = (0, 100, 0)
+                    particle_color = Color.GREEN.value
                 if event.key == pygame.K_3:
-                    color = (0, 128, 128)
+                    particle_color = Color.BLUE.value
             if event.type == pygame.MOUSEBUTTONDOWN:
                 holding_mouse = True
             if event.type == pygame.MOUSEBUTTONUP:
@@ -45,12 +47,11 @@ def main():
         if holding_mouse:
             spawn_particle()
 
-        # color = (0, random.randint(90,110), random.randint(90,110))
         update_particles()
         pygame.display.update()
 
 def spawn_particle():
-    global color
+    global particle_color
     x, y = pygame.mouse.get_pos()
     row = y // block_size
     col = x // block_size
@@ -60,13 +61,13 @@ def spawn_particle():
         pass
     else: # Spawn a particle
         tiles[row][col][1] = True
-        tiles[row][col][3] = color
+        tiles[row][col][3] = particle_color
         rect = pygame.Rect(tiles[row][col][0][0], tiles[row][col][0][1], block_size, block_size)
         pygame.draw.rect(SCREEN, tiles[row][col][3], rect)
 
 def update_particles():
 
-    global TIMER, sand_speed, color
+    global timer, sand_speed, particle_color
 
     def update_helper(row, col, row_mod = 1, col_mod = 0):
         tiles[row][col][1] = False
@@ -76,7 +77,7 @@ def update_particles():
         tiles[row+row_mod][col+col_mod][3] = tiles[row][col][3]
         to_reset.append([row+row_mod, col+col_mod])
         rect = pygame.Rect(tiles[row][col][0][0], tiles[row][col][0][1], block_size, block_size)
-        pygame.draw.rect(SCREEN, BLACK, rect)
+        pygame.draw.rect(SCREEN, Color.BLACK.value, rect)
         rect = pygame.Rect(tiles[row+row_mod][col+col_mod][0][0], tiles[row+row_mod][col+col_mod][0][1], block_size, block_size)
         pygame.draw.rect(SCREEN, tiles[row+row_mod][col+col_mod][3], rect)
         return [row+row_mod, col+col_mod]
@@ -86,7 +87,7 @@ def update_particles():
     rows = WINDOW_HEIGHT // block_size
     cols = WINDOW_WIDTH // block_size
 
-    if pygame.time.get_ticks()-TIMER > sand_speed:
+    if pygame.time.get_ticks()-timer > sand_speed:
         for i in range(rows-1, 0, -1):
             for j in range(cols-1, -1, -1):
                 if tiles[i][j][1] and tiles[i][j][2]: # We have a particle that we have not already updated
@@ -108,22 +109,18 @@ def update_particles():
                                 continue
         for reset in to_reset:
             tiles[reset[0]][reset[1]][2] = True
-        TIMER = pygame.time.get_ticks()
+        timer = pygame.time.get_ticks()
 
 def init_grid():
-
-    global tiles, block_size, color
+    global tiles, block_size, particle_color
     tiles = [[]]
     row = 0
-    col = 0
-
     for x in range(0, WINDOW_HEIGHT, block_size):
         for y in range(0, WINDOW_WIDTH, block_size):
-            tiles[row].append([(y, x), False, True, color])
-            col += 1
+            tiles[row].append([(y, x), False, True, particle_color])
         row += 1
         if x < WINDOW_HEIGHT - block_size:
             tiles.append([])
 
 if __name__ == "__main__":
-    main()
+    main(WINDOW_WIDTH, WINDOW_HEIGHT)
