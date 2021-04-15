63ba2e9bd339d9df30f9ddb166ebae96919b7a80
Anton <anton.benfey@gmail.com>
2021-04-14 22:41:21 -0300

    stuff

--- particles.py
+++ particles.py
@@ -7,20 +7,30 @@ from colors import Color
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 800
+
+GAME_SPEED = 144
+
+GREEN_RATE = 8
+BLUE_RATE = 16
+RED_RATE = 32
+
 block_size = 4
-sand_speed = 12
+known_particles = []
 particle_color = Color.BLUE.value
+current_time = pygame.time.get_ticks()
 
 def main(w, h):
-    global SCREEN, particle_color, timer
+    global GAME_SPEED, screen, particle_color, blue_timer, green_timer, red_timer
     holding_mouse = False
 
     pygame.init()
     init_grid()
 
-    SCREEN = pygame.display.set_mode((w, h))
-    timer = pygame.time.get_ticks()
-    SCREEN.fill(Color.BLACK.value)
+    screen = pygame.display.set_mode((w, h))
+    clock = pygame.time.Clock()
+    blue_timer = green_timer = red_timer = pygame.time.get_ticks()
+
+    screen.fill(Color.BLACK.value)
 
     while True:
 
@@ -29,7 +39,7 @@ def main(w, h):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     init_grid()
-                    SCREEN.fill(Color.BLACK.value)
+                    screen.fill(Color.BLACK.value)
                 if event.key == pygame.K_1:
                     particle_color = Color.RED.value
                 if event.key == pygame.K_2:
@@ -49,67 +59,106 @@ def main(w, h):
 
         update_particles()
         pygame.display.update()
+        clock.tick(GAME_SPEED)
 
 def spawn_particle():
-    global particle_color
+    global particle_color, known_particles
     x, y = pygame.mouse.get_pos()
     row = y // block_size
     col = x // block_size
 
+    def spawn_helper(r=row, c=col):
+        tiles[r][c][1] = particle_color
+        tiles[r][c][2] = True
+        rect = pygame.Rect(tiles[r][c][0][0], tiles[r][c][0][1], block_size, block_size)
+        pygame.draw.rect(screen, tiles[r][c][1], rect)
+        known_particles.append([r, c, particle_color])
+
     # If particle already on tile, do nothing
-    if tiles[row][col][1]:
+    if [row, col] in known_particles:
         pass
     else: # Spawn a particle
-        tiles[row][col][1] = True
-        tiles[row][col][3] = particle_color
-        rect = pygame.Rect(tiles[row][col][0][0], tiles[row][col][0][1], block_size, block_size)
-        pygame.draw.rect(SCREEN, tiles[row][col][3], rect)
+        if tiles[row+1][col][2] == False:
+            spawn_helper()
+        else:
+            if particle_color == Color.BLUE.value:
+                if pygame.time.get_ticks() - blue_timer > BLUE_RATE:
+                    spawn_helper()
+            elif particle_color == Color.GREEN.value:
+                if pygame.time.get_ticks() - green_timer > GREEN_RATE:
+                    spawn_helper()
+            elif particle_color == Color.RED.value:
+                if pygame.time.get_ticks() - red_timer > RED_RATE:
+                    spawn_helper()
 
 def update_particles():
 
-    global timer, sand_speed, particle_color
+    global red_timer, blue_timer, green_timer, particle_color, current_time, known_particles
 
-    def update_helper(row, col, row_mod = 1, col_mod = 0):
-        tiles[row][col][1] = False
-        tiles[row+row_mod][col+col_mod][1] = True
-        tiles[row][col][2] = True
-        tiles[row+row_mod][col+col_mod][2] = False
-        tiles[row+row_mod][col+col_mod][3] = tiles[row][col][3]
-        to_reset.append([row+row_mod, col+col_mod])
+    to_remove = []
+    to_append = []
+
+    def update_helper(row, col, row_mod, col_mod):
+        to_remove.append([row, col, tiles[row][col][1]])
+        to_append.append([row+row_mod, col+col_mod, tiles[row][col][1]])
+
+        tiles[row+row_mod][col+col_mod][1] = tiles[row][col][1]
+        tiles[row][col][2] = False
+        tiles[row+row_mod][col+col_mod][2] = True
+
         rect = pygame.Rect(tiles[row][col][0][0], tiles[row][col][0][1], block_size, block_size)
-        pygame.draw.rect(SCREEN, Color.BLACK.value, rect)
+        pygame.draw.rect(screen, Color.BLACK.value, rect)
         rect = pygame.Rect(tiles[row+row_mod][col+col_mod][0][0], tiles[row+row_mod][col+col_mod][0][1], block_size, block_size)
-        pygame.draw.rect(SCREEN, tiles[row+row_mod][col+col_mod][3], rect)
-        return [row+row_mod, col+col_mod]
+        pygame.draw.rect(screen, tiles[row+row_mod][col+col_mod][1], rect)
 
-    to_reset = []
+    def particle_helper(i, j):
+        if i == rows-1:
+            return
+        if tiles[i+1][j][2] == False:
+            update_helper(i, j, 1, 0)
+            return
+        else:
+            if j > 0:
+                if tiles[i+1][j-1][2] == False: # Is there a particle below to the left?
+                    update_helper(i, j, 1, -1)
+                    return
+            if j < cols-1:
+                if tiles[i+1][j+1][2] == False: # Is there a particle below to the right?
+                    update_helper(i, j, 1, 1)
+                    return
 
     rows = WINDOW_HEIGHT // block_size
     cols = WINDOW_WIDTH // block_size
+    reset_blue = False
+    reset_green = False
+    reset_red = False
 
-    if pygame.time.get_ticks()-timer > sand_speed:
-        for i in range(rows-1, 0, -1):
-            for j in range(cols-1, -1, -1):
-                if tiles[i][j][1] and tiles[i][j][2]: # We have a particle that we have not already updated
-                    # Are we at border?
-                    if i == rows-1:
-                        continue
-                    if tiles[i+1][j][1] == False: # Is there a particle below?
-                        # If not, move the particle down
-                        to_reset.append(update_helper(i, j, 1, 0))
-                        continue
-                    else:
-                        if j > 0:
-                            if tiles[i+1][j-1][1] == False: # Is there a particle below to the left?
-                                to_reset.append(update_helper(i, j, 1, -1))
-                                continue
-                        if j < cols-1:
-                            if tiles[i+1][j+1][1] == False: # Is there a particle below to the right?
-                                to_reset.append(update_helper(i, j, 1, 1))
-                                continue
-        for reset in to_reset:
-            tiles[reset[0]][reset[1]][2] = True
-        timer = pygame.time.get_ticks()
+    for particle in known_particles:
+        if particle[2] == Color.BLUE.value:
+            if pygame.time.get_ticks() - blue_timer > BLUE_RATE:
+                reset_blue = True
+                particle_helper(particle[0], particle[1])
+        elif particle[2] == Color.GREEN.value:
+            if pygame.time.get_ticks() - green_timer > GREEN_RATE:
+                reset_green = True
+                particle_helper(particle[0], particle[1])
+        elif particle[2] == Color.RED.value:
+            if pygame.time.get_ticks() - red_timer > RED_RATE:
+                reset_red = True
+                particle_helper(particle[0], particle[1])
+
+    if reset_blue:
+        blue_timer = pygame.time.get_ticks()
+    if reset_green:
+        green_timer = pygame.time.get_ticks()
+    if reset_red:
+        red_timer = pygame.time.get_ticks()
+
+    for appender in to_append:
+        known_particles.append(appender)
+
+    for remover in to_remove:
+        known_particles.remove(remover)
 
 def init_grid():
     global tiles, block_size, particle_color
@@ -117,7 +166,7 @@ def init_grid():
     row = 0
     for x in range(0, WINDOW_HEIGHT, block_size):
         for y in range(0, WINDOW_WIDTH, block_size):
-            tiles[row].append([(y, x), False, True, particle_color])
+            tiles[row].append([(y, x), particle_color, False])
         row += 1
         if x < WINDOW_HEIGHT - block_size:
             tiles.append([])
